23364c7198e075bb8a144bdd29f13890e75c71c9
Patrick Amrein <amrein@ubique.ch>
2026-04-20 17:01:20 +0200

    expose extract public key

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
 pub mod x509;
 pub use x509_parser;
+
+pub enum ParseError {
+    FailedToParseX509,
+}
+pub fn extract_public_key<T: AsRef<[u8]>>(cert: T) -> Result<Vec<u8>, ParseError> {
+    let Ok((_, cert)) = x509_parser::parse_x509_certificate(cert.as_ref()) else {
+        return Err(ParseError::FailedToParseX509);
+    };
+    Ok(cert.subject_pki.subject_public_key.data.to_vec())
+}
